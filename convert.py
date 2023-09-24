5b70a366fde8c5f14baf3ada9a0bf5c666506f6c
m4gnett <39447635+3kanAlpha@users.noreply.github.com>
2023-09-24 14:58:07 +0900

    仮完成

--- convert.py
+++ convert.py
@@ -2,78 +2,99 @@ from pynput.mouse import Listener, Controller, Button
 from pynput.keyboard import Controller as KeyboardController
 from pynput.keyboard import Listener as KeyboardListener
 
-import time
+import time, threading
 
-MOUSE_FIX_POS = (600, 600)
+TRACK_PER_SECOND = 20
+ONE_FRAME_TIME = 1 / TRACK_PER_SECOND
 TSUMAMI_L_LEFT = 's'
 TSUMAMI_L_RIGHT = 'a'
 TSUMAMI_R_LEFT = ';'
 TSUMAMI_R_RIGHT = 'l'
 
-prev_x = -1
-prev_y = -1
+mouse_fix_position = (0, 0)
+
+listener_enabled = False
 
 keyboard = KeyboardController()
 mouse_controller = Controller()
 
-# マウスイベントを処理する関数
-def on_move(x, y):
-    global prev_x, prev_y
-    # print(f'x: {x}, y: {y}')
-    
-    diff_x = x - prev_x
-    diff_y = y - prev_y
-    
-    if diff_x < 0:
-        keyboard.press(TSUMAMI_L_LEFT)
-        keyboard.release(TSUMAMI_L_LEFT)
-    elif diff_x > 0:
-        keyboard.press(TSUMAMI_L_RIGHT)
-        keyboard.release(TSUMAMI_L_RIGHT)
+# マウスの位置を追跡して、移動量に応じてキーを押す
+def track_mouse():
+    while True:
+        x, y = mouse_controller.position
+        # print(f'x: {x}, y: {y}')
         
-    if diff_y < 0:
-        keyboard.press(TSUMAMI_R_LEFT)
-        keyboard.release(TSUMAMI_R_LEFT)
-    elif diff_y > 0:
-        keyboard.press(TSUMAMI_R_RIGHT)
-        keyboard.release(TSUMAMI_R_RIGHT)
+        prev_x, prev_y = mouse_fix_position
     
-    prev_x = x
-    prev_y = y
+        diff_x = x - prev_x
+        diff_y = y - prev_y
+        
+        if not listener_enabled:
+            continue
+        
+        if diff_x < 0:
+            keyboard.release(TSUMAMI_L_RIGHT)
+            keyboard.press(TSUMAMI_L_LEFT)
+        elif diff_x > 0:
+            keyboard.release(TSUMAMI_L_LEFT)
+            keyboard.press(TSUMAMI_L_RIGHT)
+        else:
+            keyboard.release(TSUMAMI_L_LEFT)
+            keyboard.release(TSUMAMI_L_RIGHT)
+            
+        if diff_y < 0:
+            keyboard.release(TSUMAMI_R_RIGHT)
+            keyboard.press(TSUMAMI_R_LEFT)
+        elif diff_y > 0:
+            keyboard.release(TSUMAMI_R_LEFT)
+            keyboard.press(TSUMAMI_R_RIGHT)
+        else:
+            keyboard.release(TSUMAMI_R_LEFT)
+            keyboard.release(TSUMAMI_R_RIGHT)
+        
+        mouse_controller.position = mouse_fix_position
+        
+        time.sleep(ONE_FRAME_TIME)
+
+mouse_tracker = threading.Thread(target=track_mouse, daemon=True)
+
+def on_press(key):
+    global listener_enabled, mouse_fix_position
+    # print(f'押されたキー: {key}')
     
-    # mouse_controller.position = (x, y)
+    # 特殊なキーが押された場合にエラーが発生するので例外処理
+    try:
+        # 押されたキーが q なら...
+        if key.char == 'q':
+            # リスナーを停止する
+            print("exiting...")
+            return False
+        elif key.char == 'o':
+            if listener_enabled:
+                print("listener disabled")
+                listener_enabled = False
+            else:
+                print("listener enabled")
+                mouse_fix_position = mouse_controller.position
+                listener_enabled = True
+    except:
+        pass
 
 def main():
-    print("waiting before start...")
-    time.sleep(5)
+    # print("waiting before start...")
+    # time.sleep(10)
     
     print("start mouse listener")
     print("press q to exit")
+    print("press o to toggle mouse listener")
 
-    # マウスリスナーを作成
-    mouse_listener = Listener(on_move=on_move)
-    mouse_listener.start()
-
-    def on_press(key):
-        # print(f'押されたキー: {key}')
-        
-        # 特殊なキーが押された場合にエラーが発生するので例外処理
-        try:
-            # 押されたキーが q なら...
-            if key.char == 'q':
-                # リスナーを停止する
-                print("exiting...")
-                mouse_listener.stop()
-                return False
-        except:
-            pass
+    mouse_tracker.start()
 
     # キーボードコントローラを作成
     keyboard_listener = KeyboardListener(on_press=on_press)
     keyboard_listener.start()
 
-    # キー入力を待機する
-    mouse_listener.join()
+    # 終了の待機
     keyboard_listener.join()
 
 if __name__ == '__main__':
